70d74e41751ba959fae63a86ef9b6f05904815af
Ammon Faust <119088185+Ammon-Faust@users.noreply.github.com>
2024-02-11 13:47:33 -0700

    add: js to add cards based on form data

--- scripts.js
+++ scripts.js
@@ -6,13 +6,31 @@ function Book(title, author, read) {
     this.read = read
 };
 
+function render() {
+    let libraryElement = document.querySelector(".library-books");
+    libraryElement.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookElement = document.createElement("div");
+        bookElement.setAttribute("id", "card");
+        bookElement.innerHTML = `
+        <div>
+            <h2 id="card-title">${book.title}</h2>
+            <h3 id="card-author">${book.author}</h3>
+            <h3 id="card-read">${book.read}</h3>
+        </div>
+        `
+        libraryElement.appendChild(bookElement);
+    }
+}
+
 function addBookToLibrary() {
     let title = document.querySelector('#book-title').value;
     let author = document.querySelector('#book-author').value;
     let read = document.querySelector('#read').value;
     let newBook = new Book(title, author, read);
     myLibrary.push(newBook);
-    console.log(myLibrary)
+    render();
 };
 
 document.querySelector("#book-form").addEventListener("submit", function(event) {
